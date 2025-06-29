a2c15eeb3f4e14c54822c7ca7f815f7609824781
Emmy <e.awetobi@gmail.com>
2025-06-29 15:26:32 +0100

    Finished the Counter Functionality

--- git/src/App.jsx
+++ git/src/App.jsx
@@ -1,20 +1,22 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [counter, setCounter] = useState(0)
+   //NOTE for useState
+  //1. counter : a variable which at first has a value of 0.
+  //2. seetCounter : a function that can change counter. {0, func}
+  // every function returns something to you
 
   return (
-    <>
-     <div style={{ height:"100vh", background:"black", color:"white" }} className='container'>
-     <h2>Counter</h2>
-     <div className="coiunter-section">
-        <button>4</button>
-        <h3>0</h3>
-        <button>-</button>
-     </div>
+    <div style={{height:"100vh", background:"black", color:"white"}} className='container'>
+      <h2>Counter</h2>
+       <div className="coiunter-section">
+          <button onClick={()=> setCounter(counter+1)}>+</button>
+          <h3>{counter}</h3>
+          <button onClick={()=> setCounter(counter-1)}>-</button>
+       </div>
     </div>
-    </>
   )
 }
 
